c5f17569d02c1c4692d3c5b96600f56d6a3e3065
hamj2539 <50338885+hamj2539@users.noreply.github.com>
2019-08-02 11:08:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'BgwE3+WatHIdb4urSmU84VkXuM4ks1QXJo/u+PKIcbg179VhfPY9+51YV2tNJNDOuvYy7StbnL1tFeMt74MVv02iZsXAwl0ahlW4b4+63YHAUlC5vLYpXotFdXUbtNxbZeT4hL81i0AREZUUtuOxWgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
